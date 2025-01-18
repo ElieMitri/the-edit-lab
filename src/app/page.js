048ec7b435de479631f8200a9a2d17491f70588a
ElieMitri <eliegmitri7@gmail.com>
2025-01-18 18:23:28 +0200

    test4

--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,40 @@
 "use client"
 
-// import Image from "next/image";
-// import Navbar from "./components/Navbar";
-// import { onAuthStateChanged } from "firebase/auth";
-// import { useEffect } from "react";
-// import { db, auth } from "../../firebase";
-// import LandingPage from "./components/LandingPage";
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { db, auth } from "../../firebase";
+import LandingPage from "./components/LandingPage";
 
 export default function Home() {
 
-  // console.log("test test");
+  console.log("test test");
   
 
-  // useEffect(() => {
-  //   // Set up the listener for authentication state changes
-  //   const unsubscribe = onAuthStateChanged(auth, (user) => {
-  //     if (user) {
-  //       // User is signed in, log the display name
-  //       console.log(user.displayName);
-  //     } else {
-  //       // User is signed out
-  //       console.log("User is signed out");
-  //     }
-  //   });
+  useEffect(() => {
+    // Set up the listener for authentication state changes
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, log the display name
+        console.log(user.displayName);
+      } else {
+        // User is signed out
+        console.log("User is signed out");
+      }
+    });
   
-  //   // Cleanup the listener when the component unmounts
-  //   return () => {
-  //     unsubscribe();
-  //   };
-  // }, []);
+    // Cleanup the listener when the component unmounts
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <>
       <div>
-        <h1>Hello World</h1>
-        {/* <Navbar />
-        <LandingPage /> */}
+        <Navbar />
+        <LandingPage />
       </div>
     </>
   );
